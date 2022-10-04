0b12c1b1c835aced597b01d262bad3fae4788497
Shafira-van <shafiravandeyli10@gmail.com>
2022-10-04 10:05:33 +0700

    perbaharui footer

---
+++ profil.js
@@ -0,0 +1,13 @@
+let siswa = {
+    nama : 'shafira',
+    umur: 17,
+    hobi : 'berenang',
+    "nomor handphone" : 085762757723
+};
+
+siswa.umur =18
+siswa["nomor handphone"] = 0819213849
+console.log(siswa);
+
+
+
